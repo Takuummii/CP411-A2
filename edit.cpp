6cceacb31bf47e550ca99c8b38abfd1df9ce614e
Takuummii <124631377+Takuummii@users.noreply.github.com>
2023-09-21 19:55:26 -0400

    Update edit.cpp

--- edit.cpp
+++ edit.cpp
@@ -25,12 +25,35 @@ GLint max(GLint x, GLint y) {
 NODE *select(GLint x, GLint y) {
 	// your implementation
     // search the object that cover the clicking point staring
+	int selected = -1;
+	int counter = 0;
+	NODE *ptr = objlist.start;
+	while (ptr->next != NULL) {
+		if (ptr->object->x1 > ptr->object->x2) {
+			int temp = ptr->object->x2;
+			ptr->object->x2 = ptr->object->x1;
+			ptr->object->x1 = temp;
+		}
+		if (ptr->object->y1 > ptr->object->y2) {
+			int temp = ptr->object->y2;
+			ptr->object->y2 = ptr->object->y1;
+			ptr->object->y1 = temp;
+		}
+		if (ptr->object->type == RECTANGLE && x <= ptr->object->x2 && x >= ptr->object->x1 && y <= ptr->object->y2 && y >= ptr->object->y1) {
+			selected = counter;
+		}
+		else if () {
 
+		}
+		counter++;
+		ptr = ptr->next;
+	}
 	return NULL;
 }
 
 void Delete(NODE **pp) {
 	// your implementation
+
 }
 
 void moveFront(NODE *p) {
